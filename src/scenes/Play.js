6a005cd8c78370617882bf957ef6bb25c02a9096
Aaron Gonzales <agonz243@ucsc.edu>
2022-10-31 21:07:43 -0700

    Added drawn cards moving to their hand position

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -29,7 +29,7 @@ class Play extends Phaser.Scene {
 
         // Create align grid for placing cards in play area
         this.aGrid = new AlignGrid({scene: this, rows: 10, cols: 9});
-        //this.aGrid.showNumbers();
+        this.aGrid.showNumbers(); // FIXME
 
         // Instantiate deck as array and the player's three cards
         this.deck = [];
@@ -37,6 +37,9 @@ class Play extends Phaser.Scene {
         this.card2;
         this.card3;
 
+        this.hand1 = this.add.rectangle(0, 0, 1, 1);
+        this.hand2 = this.add.rectangle(0, 0, 1, 1);
+        this.hand3 = this.add.rectangle(0, 0, 1, 1);
 
         // Instantiate array of card names
         this.cardNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
@@ -44,8 +47,9 @@ class Play extends Phaser.Scene {
         // Populate deck
         this.populateDeck();
 
-        // Draw three cards
+        // Draw three cards and set hand positions
         this.drawThree();
+        this.setHandPos();
 
 
 
@@ -69,12 +73,12 @@ class Play extends Phaser.Scene {
         // Add cards to deck
         for (let i = 0; i < game.settings.cardCount; i++) {
             // Create card instance with corresponding attributes
-            let card = new Card(this, this.cardNames[i], 'Dummy Suit', 'cardface');
+            let card = new Card(this, this.cardNames[i], 'Dummy Suit', 'cardface'); // FIXME
             Align.scaleToGameW(card, .1);
 
-            card.isUpsideDown = false; // Set orientation
-            card.interpretationUp = ""; // Rightside up interpretation
-            card.interpretationDown = ""; // Upside down interpretation
+            card.isUpsideDown = false; // Set orientation FIXME
+            card.interpretationUp = ""; // Rightside up interpretation FIXME
+            card.interpretationDown = ""; // Upside down interpretation FIXME
             this.deck.push(card); // Add card to deck
         }
     }
@@ -89,9 +93,45 @@ class Play extends Phaser.Scene {
         this.card2 = Phaser.Utils.Array.Remove(this.deck, this.deck[1]);
         this.card3 = Phaser.Utils.Array.Remove(this.deck, this.deck[2]);
 
+        // Place cards at respective positions FIXME
+        this.aGrid.placeAtIndex(52, this.card1);
+        this.aGrid.placeAtIndex(52, this.card2);
+        this.aGrid.placeAtIndex(52, this.card3);
+    }
+
+    // FUNCTION: Sets the positions for the three cards in the player's hand
+    setHandPos() {
         // Place cards at respective positions
-        this.aGrid.placeAtIndex(75, this.card1);
-        this.aGrid.placeAtIndex(76, this.card2);
-        this.aGrid.placeAtIndex(77, this.card3);
+        this.aGrid.placeAtIndex(75, this.hand1);
+        this.aGrid.placeAtIndex(76, this.hand2);
+        this.aGrid.placeAtIndex(77, this.hand3);
+
+        // Move cards to hand positions
+        this.tweens.add({
+            targets: this.card1,
+            x: this.hand1.x,
+            y: this.hand1.y,
+            duration: 3000,
+            ease: 'Power2',
+            completeDelay: 3000
+        });
+
+        this.tweens.add({
+            targets: this.card2,
+            x: this.hand2.x,
+            y: this.hand2.y,
+            duration: 3500,
+            ease: 'Power2',
+            completeDelay: 3000
+        });
+
+        this.tweens.add({
+            targets: this.card3,
+            x: this.hand3.x,
+            y: this.hand3.y,
+            duration: 4000,
+            ease: 'Power2',
+            completeDelay: 3000
+        });
     }
 }
